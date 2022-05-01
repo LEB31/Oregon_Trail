e9d5b4535ee6fb07f8e043599a322aa05517116c
LEB31 <93269935+LEB31@users.noreply.github.com>
2022-05-01 14:55:00 -0400

    Add files via upload

--- nbc.py
+++ nbc.py
@@ -24,21 +24,39 @@ def one():
     s = Button(start, text= "Start the Journey",fg= "#6F1F51", bg="#A4A4A4", width= 20,command=trail).place(x=250,y=250)
   
 def trail():
+    global hour
     go = Toplevel()
     go.title('On the Path')
     go.geometry('600x400')
-    go.configure(bg="#5d3d1f")
+    go.configure(bg="#e6b8a0")
     load = Image.open('trail.png')
     resize = load.resize((375, 275), Image.Resampling.LANCZOS)
     render = ImageTk.PhotoImage(resize) 
     img1 = Label(go, image=render)
     img1.image=render
     img1.place(x=0,y=0)  
-    # List of distractions
-    D = [' left to carve a pumpkin', ' joined Halloween festivities', ' got scared by a ghost', ' got scared by vampires']
-    # Names of group members
-    N = ['Zero', 'Sally', 'The Major', 'The Mummy Boy']
 
+    # Hour Count
+    H = Label(go, text="Hour: 1", bg="#e6b8a0", fg="#843c3c")
+    H.place(x=475, y=75)
+
+    # List of distractions (lose a group member)
+    D = [' left to carve a pumpkin', ' joined Halloween festivities', ' got scared by a ghost', ' got scared by vampires']
+    # List of misfourtunes (lose an hour)
+    M = ["Zero lost his bone", "Sally's stitches ripped", "The Mayor lost his hat", "The Mummy Boy unraveled"]
+    # Names of group members
+    N = ['Zero', 'Sally', 'The Mayor', 'The Mummy Boy']
+    # Random
+    RM = random.randint(1,50)
+    if RM == 16:
+        pause1 = Label(go, text= M.index(0), fg ="#843c3c", bg="#e6b8a0").place(x=280, y=150)
+    elif RM == 12:
+        pause2 = Label(go, text= M.index(1), fg ="#843c3c", bg="#e6b8a0").place(x=280, y=150)
+    elif RM == 3:
+        pause3 = Label(go, text= M.index(2), fg ="#843c3c", bg="#e6b8a0").place(x=280, y=150)
+    elif RM == 23:
+        pause4 = Label(go, text= M.index(3), fg ="#843c3c", bg="#e6b8a0").place(x=280, y=150)
+    
 
 
 # Create a window
@@ -54,5 +72,9 @@ play = Button(win, text="Play", font="Courier", bg='#fcd462', command= one)
 play.place(x=280,y=145)
 
 # Variable Nightmare Zone 
+
+# Hourly Count 
+hour = 0 
+
 
 win.mainloop()
